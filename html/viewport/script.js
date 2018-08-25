2d22e651e988b98763c13f982d277bd7f55e472f
Simon Lydell <simon.lydell@gmail.com>
2018-08-25 14:12:54 +0200

    Finally figured out how to measure the viewport

--- html/viewport/script.js
+++ html/viewport/script.js
@@ -1,26 +1,41 @@
-// @flow
+/* eslint-disable flowtype/require-valid-file-annotation */
 
-const root = document.documentElement;
+const { documentElement, body, scrollingElement } = document;
 
-if (root != null) {
-  const pre = document.createElement("pre");
+const pre = document.createElement("pre");
+body.append(pre);
 
-  pre.textContent = JSON.stringify(
-    {
+pre.textContent = JSON.stringify(
+  {
+    window: {
       innerWidth: window.innerWidth,
-      clientWidth: root.clientWidth,
-      scrollWidth: root.scrollWidth,
-      offsetWidth: root.offsetWidth,
       innerHeight: window.innerHeight,
-      clientHeight: root.clientHeight,
-      scrollHeight: root.scrollHeight,
-      offsetHeight: root.offsetHeight,
     },
-    undefined,
-    2
-  );
-
-  if (document.body != null) {
-    document.body.append(pre);
-  }
-}
+    documentElement: {
+      clientWidth: documentElement.clientWidth,
+      scrollWidth: documentElement.scrollWidth,
+      offsetWidth: documentElement.offsetWidth,
+      scrollLeftMax: documentElement.scrollLeftMax,
+      clientHeight: documentElement.clientHeight,
+      scrollHeight: documentElement.scrollHeight,
+      offsetHeight: documentElement.offsetHeight,
+      scrollTopMax: documentElement.scrollTopMax,
+    },
+    body: {
+      clientWidth: body.clientWidth,
+      scrollWidth: body.scrollWidth,
+      offsetWidth: body.offsetWidth,
+      scrollLeftMax: body.scrollLeftMax,
+      clientHeight: body.clientHeight,
+      scrollHeight: body.scrollHeight,
+      offsetHeight: body.offsetHeight,
+      scrollTopMax: body.scrollTopMax,
+    },
+    viewport: {
+      width: scrollingElement.clientWidth,
+      height: scrollingElement.clientHeight,
+    },
+  },
+  undefined,
+  2
+);
