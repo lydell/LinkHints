9aebb0c6b1a55d290fd45e0cc96bfea1a6ff873d
Simon Lydell <simon.lydell@gmail.com>
2018-10-21 00:23:47 +0200

    Allow using hints in the options page

--- src/options/main.js
+++ src/options/main.js
@@ -1,5 +1,17 @@
 // @flow
 
+// Content scripts don’t run here, but we can run them manually.
+// eslint-disable-next-line import/no-restricted-paths
+import RendererProgram from "../renderer/Program";
+// eslint-disable-next-line import/no-restricted-paths
+import WorkerProgram from "../worker/Program";
+
+const rendererProgram = new RendererProgram();
+const workerProgram = new WorkerProgram();
+
+rendererProgram.start();
+workerProgram.start();
+
 function test() {
   const container = document.createElement("div");
 
