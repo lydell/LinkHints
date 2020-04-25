daa61c12d81d0edb92a087b3dc30654bb034c268
Simon Lydell <simon.lydell@gmail.com>
2020-04-25 16:10:05 +0200

    Update npm packages

--- custom.config.example.js
+++ custom.config.example.js
@@ -12,7 +12,6 @@ module.exports = {
       // Allow accessing about:config without the warning screen.
       "general.warnOnAboutConfig=false",
       // Hide info/hint/intro bars/popups.
-      "datareporting.policy.dataSubmissionPolicyBypassNotification=true",
       "browser.urlbar.timesBeforeHidingSuggestionsHint=0",
       "browser.contentblocking.introCount=20",
     ],
