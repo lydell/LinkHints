e2fb59e3047e6c98d2edafb9442b6a82e0ec3184
Dario Gjorgjevski <dario.gjorgjevski+git@gmail.com>
2020-04-30 15:13:41 +0200

    Always mark links as clickable when selecting (#16)

--- src/worker/ElementManager.js
+++ src/worker/ElementManager.js
@@ -1438,13 +1438,6 @@ export default class ElementManager {
     }
 
     switch (element.nodeName) {
-      // Links _could_ be marked as "clickable" as well for simplicity, but
-      // marking them as "link" allows opening them in a new tab by holding alt
-      // for consistency with all other hints modes.
-      case "A":
-        return element instanceof HTMLAnchorElement
-          ? getLinkElementType(element)
-          : undefined;
       // Always consider the following elements as selectable, regardless of their
       // children, since they have special context menu items. A
       // `<canvas><p>fallback</p></canvas>` could be considered a wrapper element
@@ -1453,6 +1446,7 @@ export default class ElementManager {
       // give frames hints during regular click hints mode for that reason, but
       // unfortunately for example Twitter uses iframes for many of its little
       // widgets/embeds which would result in many unnecessary/confusing hints.
+      case "A":
       case "AUDIO":
       case "BUTTON":
       case "SELECT":
