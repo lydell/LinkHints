10f77c99f9f206eee43c7479dce191442bf4ce05
Simon Lydell <simon.lydell@gmail.com>
2021-05-01 21:36:20 +0200

    TypeScript config

--- rollup.config.js
+++ rollup.config.js
@@ -129,7 +129,7 @@ function js({ input, output } /*: { input: string, output: string } */) {
       replace({ ...makeGlobals(), preventAssignment: false }),
       sucrase({
         exclude: ["node_modules/**"],
-        transforms: ["flow", "jsx"],
+        transforms: ["typescript", "jsx"],
         // Don't add `__self` and `__source` to JSX, which Preact does not support.
         production: true,
       }),
@@ -171,7 +171,7 @@ function template(
     external: (id) => !id.startsWith("."),
     plugins: [
       sucrase({
-        transforms: ["flow", "jsx"],
+        transforms: ["typescript", "jsx"],
         production: true,
       }),
       resolve(),
