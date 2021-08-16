d3d2698698e95798deae9dbb677ce96783a531ee
Simon Lydell <simon.lydell@gmail.com>
2021-08-16 21:58:51 +0200

    Don’t run on toplevel about:blank in Firefox

--- src/worker/main.ts
+++ src/worker/main.ts
@@ -1,5 +1,10 @@
 import { fireAndForget } from "../shared/main";
 import WorkerProgram from "./Program";
 
-const program = new WorkerProgram();
-fireAndForget(program.start(), "main->WorkerProgram#start");
+// In Firefox, `match_about_blank: true` triggers even if you visit
+// `about:blank` directly, not just blank iframes and `window.open()`.
+// It makes no sense doing anything in a completely blank page.
+if (!(window.location.href === "about:blank" && window.top === window)) {
+  const program = new WorkerProgram();
+  fireAndForget(program.start(), "main->WorkerProgram#start");
+}
